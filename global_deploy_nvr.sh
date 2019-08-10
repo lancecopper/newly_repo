c1a999b0bcd4ba3e89f628159e4a6c99a077dd58
lancecopper <she47637370@gmail.com>
2019-08-10 16:12:06 +0800

    added global_deploy_nvr.sh

---
+++ global_deploy_nvr.sh
@@ -0,0 +1,21 @@
+# 解压缩，将部署包中断内容mv到上一级目录
+## cd 20190717
+## mv * ../
+
+#if [[ $UID -ne 0 && $# -ne 2 ]];then
+#    echo "e.g. \"$0 facility\""
+#    exit 1
+#fi
+
+date = 20190810
+
+# 下载部署包
+wget -O deploy$date.zip https://github.com/lancecopper/newly_repo/blob/master/nvr20190809.zip?raw=true
+unzip deploy$date.zip -d deploy$date
+
+cd deploy$date
+sudo chmod +x deploy_nvr.sh
+./deploy_nvr.sh
+
+
+
