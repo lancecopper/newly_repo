9f4ac340cba2f92baf5e2ed0f9c733747f15928d
lancecopper <she47637370@gmail.com>
2019-08-12 09:36:42 +0800

    global_deploy_nvr.sh

--- global_deploy_nvr.sh
+++ global_deploy_nvr.sh
@@ -10,10 +10,14 @@
 date = 20190810
 
 # 下载部署包
-wget -O deploy$date.zip https://github.com/lancecopper/newly_repo/blob/master/nvr20190809.zip?raw=true
-unzip deploy$date.zip -d deploy$date
+#wget -O deploy$date.zip https://github.com/lancecopper/newly_repo/blob/master/nvr20190809.zip?raw=true
+#unzip deploy$date.zip -d deploy$date
 
-cd deploy$date
+wget -O deploy20190810.zip https://github.com/lancecopper/newly_repo/blob/master/nvr20190809.zip?raw=true
+unzip deploy20190810.zip -d deploy20190810
+
+cd deploy20190810
+echo 123456 | sudo -S ls
 sudo chmod +x deploy_nvr.sh
 ./deploy_nvr.sh
 
