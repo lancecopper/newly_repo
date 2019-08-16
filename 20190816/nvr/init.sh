2915182a95539ba1d22225aaa99af677f1e7b401
lancecopper <she47637370@gmail.com>
2019-08-16 21:09:57 +0800

    20190816

---
+++ 20190816/nvr/init.sh
@@ -0,0 +1,9 @@
+echo 123456 | sudo -S ls
+sudo cp -rf lib* /usr/lib/x86_64-linux-gnu
+sudo cp -rf ./HCNetSDKCom/lib* /usr/lib/x86_64-linux-gnu
+sudo chmod +x nvr.out
+
+echo 123456 | sudo apt -y install libmysqlclient-dev
+echo 123456 | sudo apt -y install libmp4v2-dev
+echo 123456 | sudo apt install -y libboost-all-dev
+echo 123456 | sudo apt install -y libcurl4-openssl-dev
